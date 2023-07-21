9fc934345414b5d682b5b8f6176d8371d921f3a1
owen-eternal <ptarworx@gmail.com>
2023-07-21 19:57:57 +0200

    Define security group rules.

--- main.tf
+++ main.tf
@@ -97,6 +97,21 @@ resource "aws_security_group" "security-groups" {
   description = each.value
   vpc_id      = aws_vpc.vpc.id
 
+  ingress {
+    description = "SSH traffic only"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "${var.project_name}-${var.environment}-${each.key}-sg"
   }
